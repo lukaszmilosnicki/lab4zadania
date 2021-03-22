ef562ef10a28dc5c270a7ad7175326b0084560d2
ŁukaszMiłośnicki <lukasz.milosnicki@gmail.com>
2021-03-22 08:53:46 +0100

    zadania

--- main.py
+++ main.py
@@ -17,3 +17,24 @@
 # with open('tekst.txt', 'r') as x:
 #     for linia in x:
 #         print(linia, end="")
+#
+# Zad 4
+# class NaZakupy:
+#     def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed):
+#         self.nazwa_produktu = nazwa_produktu
+#         self.ilosc = ilosc
+#         self.jednostka_miary = jednostka_miary
+#         self.cena_jed = cena_jed
+#
+#     def wyswietl_produkt(self):
+#         return self.nazwa_produktu
+#     def ile_produktu(self):
+#         return f'{self.ilosc} {self.jednostka_miary}'
+#     def ile_kosztuje(self):
+#         return self.ilosc * self.cena_jed
+#
+# produkt = NaZakupy('ziemniaki', 5, 'kg', 2)
+#
+# print(produkt.wyswietl_produkt())
+# print(produkt.ile_produktu())
+# print(produkt.ile_kosztuje())
